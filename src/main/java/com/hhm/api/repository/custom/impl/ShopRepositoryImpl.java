34d0e7314b8edbbdfa5edd6220cfaab5bca98857
HieuNguyen <hieunh.th.2003@gmail.com>
2025-04-15 11:34:28 +0700

    fix: custom repo

--- src/main/java/com/hhm/api/repository/custom/impl/ShopRepositoryImpl.java
+++ src/main/java/com/hhm/api/repository/custom/impl/ShopRepositoryImpl.java
@@ -51,7 +51,7 @@ public class ShopRepositoryImpl implements ShopRepositoryCustom {
 
         criteriaQuery.append("AND s.deleted = FALSE ");
 
-        if (!Objects.isNull(request.getKeyword())) {
+        if (Objects.nonNull(request.getKeyword())) {
             criteriaQuery.append("AND (s.name LIKE :keyword OR s.address LIKE :keyword) ");
 
             values.put("keyword", QueryUtils.encodeLikeString(request.getKeyword()));
