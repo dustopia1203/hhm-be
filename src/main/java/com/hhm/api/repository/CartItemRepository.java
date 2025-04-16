ecc9071b538cb26e5d02e6724adecc4cfdcfe1b8
HieuNguyen <hieunh.th.2003@gmail.com>
2025-04-16 15:41:08 +0700

    refactor: cart entity

--- src/main/java/com/hhm/api/repository/CartItemRepository.java
+++ src/main/java/com/hhm/api/repository/CartItemRepository.java
@@ -1,9 +1,9 @@
 package com.hhm.api.repository;
 
-import com.hhm.api.model.entity.Cart;
+import com.hhm.api.model.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CartRepository extends JpaRepository<Cart, UUID> {
+public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
 }
